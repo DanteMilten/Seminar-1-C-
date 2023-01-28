0ec44f972c916ac95f6c7a8b166381bcbfdbab3f
DanteMilten <milten47@yahoo.com>
2023-01-28 20:08:24 +0300

    code 7

---
+++ 7/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает последнюю цифру этого числа.
+//	456 -> 6
+//	782 -> 2
+//	918 -> 8
+
+Console.Clear();
+Console.Write("ВВедите трехзначное число: ");
+int num = int.Parse(Console.ReadLine());
+if (num < 100 || num > 999){
+    Console.WriteLine("Введено нерпавльное число");
+    return;
+}
+int otvet = num % 10;
+Console.WriteLine($"3 цифра: {otvet} ");
